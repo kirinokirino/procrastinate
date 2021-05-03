f19145886a8167cec3e647947e537b1c7fb3e9c5
kirinokirino <kirinokirino2501@gmail.com>
2021-05-03 19:29:12 +0300

    v0.1.0

---
+++ deps.ts
@@ -0,0 +1,2 @@
+export { config } from "https://deno.land/x/dotenv@v2.0.0/mod.ts";
+export * as colors from "https://deno.land/std@0.95.0/fmt/colors.ts";
---
+++ mod.ts
@@ -0,0 +1,101 @@
+#!/usr/bin/env -S deno run --no-check --allow-read=.env,.env.defaults --allow-net=api.twitch.tv
+
+import { colors, config } from "./deps.ts";
+
+if (import.meta.main) {
+  const streamNames = ["kirinokirino"];
+  const liveStreams = await getTwitchStreams(streamNames);
+
+  if (liveStreams.length < 1) console.log("Currently noone is streaming :(");
+  else {
+    for (const stream of liveStreams) {
+      console.log(
+        colors.red(colors.bold(stream.display_name)) + " is playing " +
+          colors.black(colors.bold(stream.game)) + " with " +
+          colors.blue(String(stream.viewers)) + " viewers.",
+      );
+      console.log(colors.bold(stream.url));
+      console.log(stream.status);
+      console.log("");
+    }
+  }
+}
+
+interface liveStreamInfo {
+  // deno-lint-ignore camelcase
+  display_name: string;
+  game: string;
+  viewers: number;
+  status: string;
+  url: string;
+  // deno-lint-ignore camelcase
+  stream_type: string;
+}
+
+// Get streams
+async function getTwitchStreams(channels: string[]): Promise<liveStreamInfo[]> {
+  const appClientID = config().appClientID;
+  const acceptVersion = "application/vnd.twitchtv.v5+json";
+  const ids = await getTwitchIds(appClientID, channels);
+  if (ids.length < 1) throw Error("Got no ids!");
+  const url = "https://api.twitch.tv/kraken/streams?limit=100&channel=" + ids;
+
+  const liveStreams: liveStreamInfo[] = await fetch(url, {
+    headers: {
+      "Client-ID": appClientID,
+      "Accept": acceptVersion,
+      "Content-Type": "application/json",
+    },
+  }).then(function (response) {
+    return response.json();
+  }).then(function (json) {
+    const liveStreams = [];
+    for (const stream of json.streams) {
+      if (stream.stream_type === "live") {
+        liveStreams.push({
+          display_name: stream.channel.display_name,
+          game: stream.game,
+          viewers: stream.viewers,
+          status: stream.channel.status,
+          url: stream.channel.url,
+          stream_type: stream.stream_type,
+        } as liveStreamInfo);
+      }
+    }
+    return liveStreams;
+  }).catch(function (exception) {
+    console.log("parsing failed", exception);
+    return [];
+  });
+  return liveStreams;
+}
+
+// User to IDs
+async function getTwitchIds(
+  appClientID: string,
+  channels: string[],
+): Promise<string[]> {
+  const acceptVersion = "application/vnd.twitchtv.v5+json";
+  if (channels.length < 1) channels.push("kirinokirino"); // Kappa
+  var url = "https://api.twitch.tv/kraken/users/?login=" + channels;
+
+  const ids: string[] = await fetch(url, {
+    headers: {
+      "Client-ID": appClientID,
+      "Accept": acceptVersion,
+      "Content-Type": "application/json",
+    },
+  }).then(function (response) {
+    return response.json();
+  }).then(function (json) {
+    const ids: string[] = [];
+    for (const user of json.users) {
+      ids.push(user._id as string);
+    }
+    return ids;
+  }).catch(function (exception) {
+    console.error("parsing failed", exception);
+    return [];
+  });
+  return ids;
+}
